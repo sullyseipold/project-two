a4b2920697b80ce91853e2c45433a1c22a3085e0
mirwoz <miranda.wozmak@gmail.com>
2019-03-30 11:27:29 -0400

    added schema.sql and seeds.sql. Seems to work when you copy paste

---
+++ seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (name,username,password,email,location) VALUES ('Miranda','mirwoz','password','miranda.wozmak@gmail.com','Portsmouth,NH');
+INSERT INTO user (name,username,password,email,location) VALUES ('Jenn','JennG','password','Jen@gmail.com','Manchester,NH');
+INSERT INTO item (userid,item_name,price,image_url) VALUES ('Lamp',5.00,'www.google.com');
+INSERT INTO rental (itemid,userid,start_date,end_date) VALUES (1,1,20190331,20190402);
